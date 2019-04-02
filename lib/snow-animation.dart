9a40030d324d0e45f5cefac0ad1c212ad626cf8a
TrieuLe <longtrieu1912@gmail.com>
2019-04-02 17:53:29 +0700

    add snow animation

---
+++ lib/snow-animation.dart
@@ -0,0 +1,163 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:proximity_plugin/proximity_plugin.dart';
+
+class SnowWidget extends StatefulWidget {
+  final int totalSnow;
+  final double speed;
+  final bool isRunning;
+
+  SnowWidget({Key key, this.totalSnow, this.speed, this.isRunning})
+      : super(key: key);
+
+  _SnowWidgetState createState() => _SnowWidgetState();
+}
+
+class _SnowWidgetState extends State<SnowWidget>
+    with SingleTickerProviderStateMixin {
+  Random _rnd;
+  AnimationController controller;
+  Animation animation;
+  List<Snow> _snows;
+  double angle = 0;
+  double W = 0;
+  double H = 0;
+  @override
+  void initState() {
+    super.initState();
+    this.init();
+  }
+
+  init() {
+    _rnd = new Random();
+    if (controller == null) {
+      controller = new AnimationController(
+          lowerBound: 0,
+          upperBound: 1,
+          vsync: this,
+          duration: const Duration(milliseconds: 20000));
+      controller.addListener(() {
+        if (mounted) {
+          setState(() {
+            update();
+          });
+        }
+      });
+    }
+    if (!widget.isRunning) {
+      controller.stop();
+    } else {
+      controller.repeat();
+    }
+
+    proximityEvents.listen((ProximityEvent event) {
+      print(event);
+    });
+  }
+
+  @override
+  dispose() {
+    controller.stop();
+    super.dispose();
+  }
+
+  _createSnow() {
+    _snows = new List();
+    for (var i = 0; i < widget.totalSnow; i++) {
+      _snows.add(new Snow(
+          x: _rnd.nextDouble() * W,
+          y: _rnd.nextDouble() * H,
+          r: _rnd.nextDouble() * 4 + 1,
+          d: _rnd.nextDouble() * widget.speed));
+    }
+  }
+
+  update() {
+    angle += 0.01;
+    if (widget.totalSnow != _snows.length) {
+      _createSnow();
+    }
+    for (var i = 0; i < widget.totalSnow; i++) {
+      var snow = _snows[i];
+      //We will add 1 to the cos function to prevent negative values which will lead flakes to move upwards
+      //Every particle has its own density which can be used to make the downward movement different for each flake
+      //Lets make it more random by adding in the radius
+      snow.y += cos(angle + snow.d) + widget.speed + snow.r / 2;
+      snow.x += sin(angle) * 2;
+      if (snow.x > W + 5 || snow.x < -5 || snow.y > H) {
+        if (i % 3 > 0) {
+          //66.67% of the flakes
+          _snows[i] =
+              new Snow(x: _rnd.nextDouble() * W, y: -10, r: snow.r, d: snow.d);
+        } else {
+          //If the flake is exitting from the right
+          if (sin(angle) > 0) {
+            //Enter from the left
+            _snows[i] =
+                new Snow(x: -5, y: _rnd.nextDouble() * H, r: snow.r, d: snow.d);
+          } else {
+            //Enter from the right
+            _snows[i] = new Snow(
+                x: W + 5, y: _rnd.nextDouble() * H, r: snow.r, d: snow.d);
+          }
+        }
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_snows == null) {
+      W = MediaQuery.of(context).size.width;
+      H = MediaQuery.of(context).size.height;
+      _createSnow();
+    }
+    return Container(
+      child: CustomPaint(
+        willChange: widget.isRunning,
+        painter: SnowPainter(
+            progress: controller.value,
+            isRunning: widget.isRunning,
+            snows: _snows),
+        size: Size.infinite,
+      ),
+    );
+  }
+}
+
+class Snow {
+  double x;
+  double y;
+  double r; //radius
+  double d; //density
+  Snow({this.x, this.y, this.r, this.d});
+}
+
+class SnowPainter extends CustomPainter {
+  List<Snow> snows;
+  bool isRunning;
+  double progress = 0;
+
+  SnowPainter({this.progress, this.isRunning, this.snows});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    if (snows == null) return;
+    //draw circle
+    final Paint paint = new Paint()
+      ..color = Colors.white
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 10.0;
+    for (var i = 0; i < snows.length; i++) {
+      var snow = snows[i];
+      if (snow != null) {
+        canvas.drawCircle(Offset(snow.x, snow.y), snow.r, paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(SnowPainter oldDelegate) =>
+      oldDelegate.progress != progress && isRunning;
+}
